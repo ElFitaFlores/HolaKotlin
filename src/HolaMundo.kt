655ee682da8334223dae299098200714f90300f5
Rafael Flores <fita500@gmail.com>
2020-07-06 20:08:08 -0600

    Add operators

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
     println("Hola mundo")
+    val a = 4
+    val b = 2
+    println(a.plus(b))
+    println(a.minus(b))
+    println(a.times(b))
+    println(a.div(b))
 }
