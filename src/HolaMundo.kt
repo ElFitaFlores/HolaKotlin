c7fe1c35fb1bc154e8d18c7614c6407d611c8b39
Rafael Flores <fita500@gmail.com>
2020-07-14 18:52:24 -0600

    Strings

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -6,4 +6,21 @@ fun main(args: Array<String>) {
     println(a.minus(b))
     println(a.times(b))
     println(a.div(b))
+
+    val name = args[0]
+    println(name)
+
+    val nombre = "Rafael"
+    val apellido: String = "Flores"
+
+    println("Tu nombre es $nombre $apellido")
+
+    //Raw strings
+    """
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non lorem euismod urna cursus elementum. 
+        Ut ipsum elit, condimentum eu cursus id, placerat at ligula. Praesent sodales elit ac sagittis malesuada. Duis sodales, 
+        diam ut pretium ullamcorper, nibh ex iaculis diam, blandit elementum mauris lectus ut turpis. Morbi nec arcu sed libero 
+        convallis consequat eget nec dolor. Nam vel est ex. Phasellus venenatis rhoncus nunc, sit amet aliquet metus 
+        sollicitudin eu.
+    """.trimIndent()
 }
